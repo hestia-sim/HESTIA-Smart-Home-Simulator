bfc912f34ae5fcd21b8b5e53ec3f73f2af3f49ab
mayki <maykioliveira@ufba.br>
2024-01-25 12:37:52 -0300

    ajuste

--- Testes_Rotina.py
+++ Testes_Rotina.py
@@ -11,7 +11,7 @@ def calcula_horario_atividade(rotina, atividades):
 
 with open('./rotinas/Grafo_casa-ROTINA_ARTIGO(2usuarios_bemDefinido_semvariacao_02).json') as json_file:
     dados = json.load(json_file)
-    rotina_dia = ["ACORDAR_QUARTO_01", "ESCOVAR_DENTES_BANHEIRO_02", "CAFE_DA_MANHA", "TRABALHAR_FORA"]
+    rotina_dia = ["ACORDAR_QUARTO_01", "CAFE_DA_MANHA", "USAR_BANHEIRO_01", "FAZER_NADA"]
     # ["ACORDAR_QUARTO_01", "ESCOVAR_DENTES_BANHEIRO_02", "TRABALHAR_FORA", "PASSEAR_30_MINUTOS", "PASSEAR_30_MINUTOS", "ASSISTIR_TV_120_MINUTOS", "JANTAR", "ASSISTIR_TV_120_MINUTOS", "TOMAR_BANHO_BANHEIRO_02","DORMIR_QUARTO_01"]
 
     
